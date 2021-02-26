45a7a4977b396369d0f1fd969af6e914b1b63829
Armneogs <33708099+armneogs@users.noreply.github.com>
2021-02-26 11:38:45 +0700

    Initial commit

---
+++ src/main/java/com/realcozy/TestArmyApplication.java
@@ -0,0 +1,13 @@
+package com.realcozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestArmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestArmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realcozy/TestArmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realcozy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestArmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
